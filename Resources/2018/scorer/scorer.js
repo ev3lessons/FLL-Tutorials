163101e8150ef920cd2cbc7f3d9b110b185c9b2a
Sanjay Seshan <sanjay.seshan@outlook.com>
2018-10-15 19:52:27 -0400

    walk save scorer fix

--- Resources/2018/scorer/scorer.js
+++ Resources/2018/scorer/scorer.js
@@ -142,7 +142,6 @@ function recalc(points,mission,saveValue){
   }
 
   document.getElementById('allpoints').innerHTML = allmission
-  document.getElementById('allpoints2').innerHTML = allmission
 
     var ranges = $("input[data-type='range']");
     var radios = $("input[type='radio']");
